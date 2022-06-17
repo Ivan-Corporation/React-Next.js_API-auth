6e59a5423a5af63eb62e9f958b2bdf8ffc9bcbc7
Ivan <hammer.99.ivan@gmail.com>
2022-06-17 18:59:39 +0700

    deploy vercel fix 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { GradientBackground } from 'gradient-background'
 import '../styles/globals.css'
 import '../styles/tutorial_button.css'
+import 'intro.js/introjs.css';
 
 function MyApp({ Component, pageProps }) {
   return (
